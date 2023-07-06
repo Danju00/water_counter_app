f288bea1d4dbfca826aa3087c1678d39ffe1c179
Danju00 <leinad9265@gmail.com>
2023-07-06 11:19:51 -0600

    start_screen

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,67 +1,137 @@
-import 'dart:html';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatefulWidget {
-  const StartScreen({Key? key}) : super(key: key);
+ const StartScreen({Key? key}) : super(key: key);
 
   @override
   State<StartScreen> createState() => _StartScreenState();
 }
 
-class _StartScreenState extends State<StartScreen> {
+class  _StartScreenState extends State <StartScreen> {
+
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _totalMl(),
+        _indicators(),
+        _buttonReg(),
+
+      ],
+    ),
+    backgroundColor: Colors.black, 
+    );
+  }
+  Widget _totalMl(){
+    
+      return Column(
         children: [
-          _totalM1(),
-          _indicators(),
-          _buttonReg(),
+          Text(
+            '0 mL',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          Text(
+            'Faltan 2500 mL',
+            style: Theme.of(context). textTheme.bodyMedium,
+          )
         ],
+      );
+  }
+  Widget _indicators () {
+    return IntrinsicHeight(
+      child:
+          Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _percentage(),
+            const SizedBox(
+              width: 10,
+            ),
+            _smallDividier(),
+            const SizedBox(
+              width: 10,
+            ),
+            _hydration(),
+          ],
+        ),
+    );
+  }
+Widget _percentage(){
+            return Column(
+              children:[
+                SizedBox(
+                  height: 60,
+                  child: Stack(
+                    alignment: Alignment.center,
+                children: [
+                 const SizedBox(
+                  width: 45,
+                  height: 45,
+                   child: CircularProgressIndicator(
+                    strokeWidth: 6,
+                    value: 0.3,
+                    color: Color.fromARGB(255, 62, 139, 236),
+                    backgroundColor: Color.fromARGB(255, 17, 50, 74),
+                ),
+                 ),
+                Text(
+              '0 %',
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+          ],
+        ),
       ),
-    );
-  }
+            Text(
+              'Hoy',
+              style: Theme.of(context).textTheme.bodySmall,
+              textAlign: TextAlign.center,
+            )
+          ],
+        );
+            
+}
 
-  Widget _totalM1() {
-    return Column(
-      children: [
-        Text(
-          '0 mL',
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
-        Text(
-          'Faltan 2500mL',
-          style: Theme.of(context).textTheme.bodyMedium,
-        ),
-      ],
-    );
-  }
+Widget _smallDividier(){
+  return const VerticalDivider(
+    color: Color.fromARGB(255, 51, 51, 51),
+    thickness: 2,
+    width: 20,
+    indent: 10,
+    endIndent: 0,
+  );
 
-  Widget _indicators() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Text(
-          '0 %',
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
-        Text(
-          '1.0',
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
-      ],
-    );
-  }
+}
 
+Widget _hydration(){
+  return Column(
+    children: [
+      _intervalBar(),
+      Text(
+        'Hidratación',
+        style: Theme.of(context).textTheme.bodySmall,
+      ),
+    ],
+  );
+}
+
+Widget _intervalBar(){
+  return Text(
+              '1.0',
+              style: Theme.of(context).textTheme.headlineSmall,
+            );
+
+}
   Widget _buttonReg() {
-    return Center(
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(),
-        onPressed: () {},
-        child: const Text('Registrar'),
-      ),
-    );
+     return Center(
+          child: ElevatedButton(
+            onPressed: () { },
+           child: const Text('Registrar'),
+           ), 
+        );
   }
 }
